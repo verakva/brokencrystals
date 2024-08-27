dc507c58952d295df01294af9e04ec72ca781382
Viachaslau Tyshkavets <pmstss@gmail.com>
2024-08-27 15:01:06 +0400

    test: proper deps mocks in jest unit tests, add unit tests to ci

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,43 @@
+import * as fs from 'fs';
+import { EntityManager } from '@mikro-orm/core';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpClientService } from '../httpclient/httpclient.service';
+import { KeyCloakService } from '../keycloak/keycloak.service';
 import { AuthService } from './auth.service';
+
+jest.mock('fs');
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    (fs.readFileSync as jest.Mock) = jest.fn((path: string) =>
+      path.toLowerCase().includes('json') ? '{}' : 'mocked-key-content',
+    );
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: EntityManager,
+          useValue: {},
+        },
+        {
+          provide: KeyCloakService,
+          useValue: {},
+        },
+        {
+          provide: HttpClientService,
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: (key: string) => key,
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
